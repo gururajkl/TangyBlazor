24735038a0e2d8c258448165049405cad8ec167c
Gururaj K L <EE212551@sasken.com>
2023-12-20 21:31:14 +0530

    Remove unused usings.

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tangy_Models
+﻿namespace Tangy_Models
 {
     public class CategoryDTO
     {
